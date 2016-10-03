3e69a02028d09f365142871e54c8e49dcaeca682
grafexum <charlesexum@gmail.com>
2016-10-03 11:37:53 -0400

    Add files via upload
    A base working state.

---
+++ SlashTell/SlashTellClient/SlashTellClient/frmSlashTellClient.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSlashTellClient
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.txtMessage = New System.Windows.Forms.TextBox()
+        Me.txtClientConsole = New System.Windows.Forms.TextBox()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lvlHostIP = New System.Windows.Forms.Label()
+        Me.txtIPAddress = New System.Windows.Forms.TextBox()
+        Me.lblPort = New System.Windows.Forms.Label()
+        Me.nudPortNumber = New System.Windows.Forms.NumericUpDown()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.nudPortNumber, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnConnect.Location = New System.Drawing.Point(445, 12)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 7
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'btnSend
+        '
+        Me.btnSend.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSend.Location = New System.Drawing.Point(445, 451)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(75, 20)
+        Me.btnSend.TabIndex = 6
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtMessage.Location = New System.Drawing.Point(12, 451)
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(427, 20)
+        Me.txtMessage.TabIndex = 5
+        '
+        'txtClientConsole
+        '
+        Me.txtClientConsole.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtClientConsole.Location = New System.Drawing.Point(12, 41)
+        Me.txtClientConsole.Multiline = True
+        Me.txtClientConsole.Name = "txtClientConsole"
+        Me.txtClientConsole.ReadOnly = True
+        Me.txtClientConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtClientConsole.ShortcutsEnabled = False
+        Me.txtClientConsole.Size = New System.Drawing.Size(400, 404)
+        Me.txtClientConsole.TabIndex = 4
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(55, 12)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(100, 20)
+        Me.txtUserName.TabIndex = 8
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(14, 15)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(35, 13)
+        Me.lblName.TabIndex = 9
+        Me.lblName.Text = "Name"
+        '
+        'lvlHostIP
+        '
+        Me.lvlHostIP.AutoSize = True
+        Me.lvlHostIP.Location = New System.Drawing.Point(161, 15)
+        Me.lvlHostIP.Name = "lvlHostIP"
+        Me.lvlHostIP.Size = New System.Drawing.Size(17, 13)
+        Me.lvlHostIP.TabIndex = 10
+        Me.lvlHostIP.Text = "IP"
+        '
+        'txtIPAddress
+        '
+        Me.txtIPAddress.Location = New System.Drawing.Point(184, 11)
+        Me.txtIPAddress.Name = "txtIPAddress"
+        Me.txtIPAddress.Size = New System.Drawing.Size(100, 20)
+        Me.txtIPAddress.TabIndex = 11
+        Me.txtIPAddress.Text = "127.0.0.1"
+        '
+        'lblPort
+        '
+        Me.lblPort.AutoSize = True
+        Me.lblPort.Location = New System.Drawing.Point(290, 15)
+        Me.lblPort.Name = "lblPort"
+        Me.lblPort.Size = New System.Drawing.Size(26, 13)
+        Me.lblPort.TabIndex = 12
+        Me.lblPort.Text = "Port"
+        '
+        'nudPortNumber
+        '
+        Me.nudPortNumber.Location = New System.Drawing.Point(322, 11)
+        Me.nudPortNumber.Maximum = New Decimal(New Integer() {65000, 0, 0, 0})
+        Me.nudPortNumber.Name = "nudPortNumber"
+        Me.nudPortNumber.Size = New System.Drawing.Size(95, 20)
+        Me.nudPortNumber.TabIndex = 13
+        Me.nudPortNumber.Value = New Decimal(New Integer() {9077, 0, 0, 0})
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(418, 41)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(102, 404)
+        Me.TextBox1.TabIndex = 14
+        '
+        'frmSlashTellClient
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(532, 483)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.nudPortNumber)
+        Me.Controls.Add(Me.lblPort)
+        Me.Controls.Add(Me.txtIPAddress)
+        Me.Controls.Add(Me.lvlHostIP)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Controls.Add(Me.txtClientConsole)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(548, 522)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(548, 522)
+        Me.Name = "frmSlashTellClient"
+        Me.Text = "SlashTell -- Client"
+        CType(Me.nudPortNumber, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnConnect As System.Windows.Forms.Button
+    Friend WithEvents btnSend As System.Windows.Forms.Button
+    Friend WithEvents txtMessage As System.Windows.Forms.TextBox
+    Friend WithEvents txtClientConsole As System.Windows.Forms.TextBox
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lvlHostIP As System.Windows.Forms.Label
+    Friend WithEvents txtIPAddress As System.Windows.Forms.TextBox
+    Friend WithEvents lblPort As System.Windows.Forms.Label
+    Friend WithEvents nudPortNumber As System.Windows.Forms.NumericUpDown
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ SlashTell/SlashTellClient/SlashTellClient/frmSlashTellClient.vb
@@ -0,0 +1,186 @@
+﻿Imports System.Threading
+Imports System.Net.Sockets
+Imports System.Text
+
+Public Class frmSlashTellClient
+
+    Private co_Client As TcpClient
+    Private ci_PortNumber As Integer = 9077
+    Private cs_IPAddress As String = "127.0.0.1"
+    Private cs_User As String = "[Bart Simpson]"
+    Private cs_Message As String
+
+
+    Private ReturningMessage As String
+    ' Private InfCounter As Integer
+    Private c_bKeepListening As Boolean
+    Private ServerStream As NetworkStream
+
+    Private ClientThread As Threading.Thread
+
+    Private Sub PostToClientConsole(PostMe As String)
+
+        Dim mo_s As String = PostMe
+        'PostMe &= DateTime.Now.ToString("hh:mm:ss") & " " & cs_User & " " & PostMe
+        txtClientConsole.AppendText(mo_s & vbCrLf)
+        'txtClientConsole.Text &= vbCrLf
+        'txtClientConsole.Text &= DateTime.Now.ToString("hh:mm:ss") & " " & cs_User & " " & PostMe
+    End Sub
+
+    Private Sub btnConnect_Click(sender As System.Object, e As System.EventArgs) Handles btnConnect.Click
+        Connect()
+    End Sub
+
+    Private Function SetPortNumber() As Integer
+        ci_PortNumber = nudPortNumber.Value
+        Return ci_PortNumber
+    End Function
+
+    Private Function SetUserName() As String
+
+        If txtUserName.Text <> "" Then
+            cs_User = txtUserName.Text
+        Else
+            cs_User = "Lemming"
+            txtUserName.Text = cs_User
+        End If
+
+        Return cs_User
+    End Function
+
+    Private Function InitTcpClient() As TcpClient
+        co_Client = New TcpClient()
+        'Later on pull ip and user name from the form.
+        Return co_Client
+    End Function
+
+    Private Sub Connect()
+
+        If c_bKeepListening Then
+            PostToClientConsole("Disconnecting: " & ci_PortNumber)
+            c_bKeepListening = False
+            Me.btnConnect.Text = "Connect"
+        Else
+            InitTcpClient()
+            SetUserName()
+            SetPortNumber()
+
+            c_bKeepListening = True
+            ClientThread = New Threading.Thread(AddressOf ClientListeningLoop)
+            ClientThread.Start()
+
+            co_Client.Connect(cs_IPAddress, ci_PortNumber)
+            Transmit(cs_User)
+            PostToClientConsole("Using IP: " & cs_IPAddress)
+            PostToClientConsole("Connection Established on port: " & ci_PortNumber)
+            Me.btnConnect.Text = "Disconnect"
+        End If
+
+    End Sub
+
+    Private Function GetMessage() As String
+        cs_Message = txtMessage.Text
+        txtMessage.Text = ""
+        Return cs_Message
+    End Function
+
+    Private Sub Transmit(Username As String)
+        'Pull directly from form and stuff into string until I find a more elegant way to handle this.
+        'Dim mo_StringToSend As String = DateTime.Now.ToString("hh:mm:ss") & " " & cs_User & ": "
+        'mo_StringToSend = mo_StringToSend & GetMessage()
+
+        'If mo_StringToSend = "" Then
+        '    mo_StringToSend &= " "
+        'End If
+
+        Dim mo_Stream As NetworkStream = co_Client.GetStream()
+
+        If mo_Stream.CanRead And mo_Stream.CanWrite Then
+            'Send a string to the server.
+            Dim mo_OutgoingBytes As [Byte]() = Encoding.ASCII.GetBytes(Username)
+            mo_Stream.Write(mo_OutgoingBytes, 0, mo_OutgoingBytes.Length)
+            mo_Stream.Flush()
+
+            'Get the echo from server
+            'Dim mo_IncomingBytes(co_Client.ReceiveBufferSize) As Byte
+            'mo_Stream.Read(mo_IncomingBytes, 0, co_Client.ReceiveBufferSize)
+            'Dim mo_IncomingString As String = Encoding.ASCII.GetString(mo_IncomingBytes)
+            'PostToClientConsole(mo_IncomingString)
+
+        End If
+
+    End Sub
+
+    Private Sub Transmit()
+        'Pull directly from form and stuff into string until I find a more elegant way to handle this.
+        Dim mo_StringToSend As String = DateTime.Now.ToString("hh:mm:ss") & " " & cs_User & ": "
+        mo_StringToSend = mo_StringToSend & GetMessage()
+
+        If mo_StringToSend = "" Then
+            mo_StringToSend &= " "
+        End If
+
+        Dim mo_Stream As NetworkStream = co_Client.GetStream()
+
+        If mo_Stream.CanRead And mo_Stream.CanWrite Then
+            'Send a string to the server.
+            Dim mo_OutgoingBytes As [Byte]() = Encoding.ASCII.GetBytes(mo_StringToSend)
+            mo_Stream.Write(mo_OutgoingBytes, 0, mo_OutgoingBytes.Length)
+            mo_Stream.Flush()
+
+            'Get the echo from server
+            'Dim mo_IncomingBytes(co_Client.ReceiveBufferSize) As Byte
+            'mo_Stream.Read(mo_IncomingBytes, 0, co_Client.ReceiveBufferSize)
+            'Dim mo_IncomingString As String = Encoding.ASCII.GetString(mo_IncomingBytes)
+            'PostToClientConsole(mo_IncomingString)
+
+        End If
+
+    End Sub
+
+    Private Sub ClientListeningLoop()
+        Dim mo_bytes(1024) As [Byte]
+        Dim mo_data As [String] = Nothing
+        Dim mo_Stream As NetworkStream
+        Dim mo_i As Int32
+
+        Do While c_bKeepListening
+            mo_Stream = co_Client.GetStream()
+            mo_i = mo_Stream.Read(mo_bytes, 0, mo_bytes.Length)
+            mo_data = System.Text.Encoding.ASCII.GetString(mo_bytes, 0, mo_i)
+
+            ReturningMessage = mo_data
+            msg()
+            'Dim buffsize As Integer
+            'Dim inStream(1024) As Byte
+            'buffsize = co_Client.ReceiveBufferSize()
+            'ServerStream.Read(inStream, 0, buffsize)
+            'Dim IncomingData As String = Encoding.ASCII.GetString(inStream)
+            'PostToClientConsole(IncomingData)
+        Loop
+
+        MsgBox("Exiting ReadLoop")
+
+    End Sub
+
+    Private Sub msg()
+        If Me.InvokeRequired Then
+            Me.Invoke(New MethodInvoker(AddressOf msg))
+        Else
+            txtClientConsole.AppendText(ReturningMessage)
+            txtClientConsole.Text &= vbCrLf
+            'PostToClientConsole(ReturningMessage)
+        End If
+    End Sub
+
+    Private Sub btnSend_Click(sender As System.Object, e As System.EventArgs) Handles btnSend.Click
+        Transmit()
+    End Sub
+
+    Private Sub txtMessage_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles txtMessage.KeyPress
+        If e.KeyChar = Chr(13) Then
+            Transmit()
+        End If
+    End Sub
+
+End Class
